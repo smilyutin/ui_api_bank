7a64a75829cc15da0252345c5fdfe3ab222d4b61
smilyutin <53317317+smilyutin@users.noreply.github.com>
2026-07-08 18:38:13 -0700

    refactor: Remove unused barrel exports and update README for clarity on schema generation and test plans

--- fixtures/api/types.ts
+++
@@ -1,4 +0,0 @@
-export type { User } from '../../helpers/credentials';
-export type { CreatePayload, TriedEntry, CreateResult } from './create-user.helpers';
-export type { LoginAttempt, LoginFlowResult } from './login.helpers';
-export type { RegisterFormPayload, RegisterFormSubmission } from './register-form.helpers';
--- helpers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './auth-bootstrap';
-export * from './credentials';
-export * from './performance-metrics';
-export * from './schema-validator';
